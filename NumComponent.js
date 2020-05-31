e70b4800c11275654646b70b9dd1e14287c3e1a3
Jens-Horstmann <Jens_Horstmann@gmx.net>
2020-05-31 18:24:10 +0800

    Add files via upload

---
+++ NumComponent.js
@@ -0,0 +1,16 @@
+
+class NumComponent extends Rete.Component {
+  constructor() {
+    super('Number');
+  }
+
+  builder(node) {
+    let out = new Rete.Output('num', 'Number', numSocket);
+
+    node.addOutput(out);
+  }
+
+  worker(node, inputs, outputs) {
+    outputs['num'] = node.data.num;
+  }
+}
